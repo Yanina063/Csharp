e0dee6a68d5061a727d14da09bed39da8cd908da
Yanina063 <strrrekosa@mail.ru>
2022-05-20 18:53:34 +0400

    Сравнение чисел Max и Min

---
+++ Practic2/Program.cs
@@ -0,0 +1,20 @@
+﻿//Сравнить два числа
+int a = new Random().Next(1,10);
+Console.WriteLine(a);
+
+int b = new Random().Next(1,10);
+Console.WriteLine(b);
+if(a > b) 
+{
+    int max = a;
+    int min = b;
+}
+Console.WriteLine("Максимум a = ");
+if (b > a) 
+{
+    int max = b;
+    int min = a;
+}
+Console.WriteLine("Максимум max = , min = ");
+Console.WriteLine("Максимум max > min");
+
