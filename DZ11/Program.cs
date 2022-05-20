ac6ce6162e6250b241b327bd79319b7c7787540d
Yanina063 <strrrekosa@mail.ru>
2022-05-20 23:06:45 +0400

    Является первое число квадратом второго

---
+++ DZ11/Program.cs
@@ -0,0 +1,18 @@
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+Console.Write("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+
+int c = b * b;
+Console.WriteLine(c);
+
+if (a == c)
+ {
+   Console.Write("a является квадратом числа b");
+   Console.WriteLine(a); 
+ }
+else {
+    Console.Write("Число a не является квадратом b");
+}
+
