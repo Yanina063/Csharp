b70b0ce7071edd41272568c5c8a8fc8851e55c1e
Yanina063 <strrrekosa@mail.ru>
2022-05-20 20:22:16 +0400

    Сравнение трех чисел

---
+++ Practic3/Program.cs
@@ -0,0 +1,39 @@
+﻿// Сравнить три числа
+//Сравнить два числа
+
+int a = new Random().Next(1,10);
+Console.WriteLine(a);
+
+int b = new Random().Next(1,10);
+Console.WriteLine(b);
+
+int d = new Random().Next(1,10);
+Console.WriteLine(d);
+
+int max = a;
+if(b > max) {
+    max = b;
+}
+if(d > max) {
+    max = d;
+}
+int min = a;
+if(b < min) {
+    min = b;
+}
+if(d < min) {
+    min = d;
+}
+
+int mid = a;
+if(max > b && b > min) {
+    mid = b;
+}
+if(max > d && d > min) {
+    mid = d;
+}
+
+Console.WriteLine($"Max: {max}");
+Console.WriteLine($"Min: {min}");
+Console.WriteLine($"Mid: {mid}");
+Console.WriteLine($"{max} > {mid} > {min}");
