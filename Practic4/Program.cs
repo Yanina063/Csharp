447a767da3a1f73bdda49ee292ba472b1271900f
Yanina063 <strrrekosa@mail.ru>
2022-05-20 21:39:37 +0400

    Выборка чисел от -n до n

---
+++ Practic4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти все нечетные числа о -n до n
+Console.WriteLine("Введите число: ");
+int n = new Random().Next(-10,10); //-9 -8 -7 ...0...7 8 9
+Console.WriteLine(n);
+int min = -n;
+
+while(min <= n)
+{
+    Console.Write(" " + min);
+    min ++;
+}
