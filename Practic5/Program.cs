5e888e2e9a0d90f43b8da4c40e3c457ea91823f5
Yanina063 <strrrekosa@mail.ru>
2022-05-20 22:09:01 +0400

    Добавили проверку числа на трехзначность

--- Practic5/Program.cs
+++ Practic5/Program.cs
@@ -1,5 +1,11 @@
 ﻿// Ввести трехзначное число и вывести последюю цифру
 Console.Write("Введите трехзначное число: ");
  int a = int.Parse(Console.ReadLine());
-Console.Write("Последняя цифра 3-х значного числа = ");
-Console.WriteLine(a % 10);
+ if (a > 99 && a < 1000)
+ {
+   Console.Write("Последняя цифра 3-х значного числа = ");
+   Console.WriteLine(a % 10);  
+ }
+else {
+    Console.Write("Число не 3-х значное");
+}
