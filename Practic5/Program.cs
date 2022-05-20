b031849414257da0f4b0745e30902c8087896115
Yanina063 <strrrekosa@mail.ru>
2022-05-20 21:56:44 +0400

    Ввести трехзначное число и вывести последюю цифру

---
+++ Practic5/Program.cs
@@ -0,0 +1,5 @@
+﻿// Ввести трехзначное число и вывести последюю цифру
+Console.Write("Введите трехзначное число: ");
+ int a = int.Parse(Console.ReadLine());
+Console.Write("Последняя цифра 3-х значного числа = ");
+Console.WriteLine(a % 10);
