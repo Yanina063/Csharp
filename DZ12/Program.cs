a4673914c93882a19872f2cc20b7a43229b9f6b8
Yanina063 <strrrekosa@mail.ru>
2022-05-21 13:23:20 +0400

    День недели заданному номеру

---
+++ DZ12/Program.cs
@@ -0,0 +1,44 @@
+﻿// Позаданному номеру дня недели вывести его название
+Console.Write("Ввести номер дня недели от 1 до 7 ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine(a);
+if (a == 1)
+ {
+   Console.Write("a соответствует дню недели");
+   Console.WriteLine(" понедельник"); 
+ }
+if (a == 2)
+ {
+   Console.Write("a соответствует дню недели");
+   Console.WriteLine(" вторник"); 
+ }
+ if (a == 3)
+ {
+   Console.Write("a соответствует дню недели");
+   Console.WriteLine(" среда"); 
+ }
+ if (a == 4)
+ {
+   Console.Write("a соответствует дню недели");
+   Console.WriteLine(" четверг"); 
+ }
+ if (a == 5)
+ {
+   Console.Write("a соответствует дню недели");
+   Console.WriteLine(" пятница"); 
+ }
+ if (a == 6)
+ {
+   Console.Write("a соответствует дню недели");
+   Console.WriteLine(" суббота"); 
+ }
+ if (a == 7)
+ {
+   Console.Write("a соответствует дню недели");
+   Console.WriteLine(" воскресенье"); 
+ }
+
+string [] days = { "понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье" };
+string day = days[a - 1];
+Console.WriteLine($"Наш день недели из массива: {day}");
