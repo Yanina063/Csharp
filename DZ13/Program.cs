8a1f98947b730d931c5320f62e4f7e0dc87e13c6
Yanina063 <strrrekosa@mail.ru>
2022-05-21 13:32:35 +0400

    Выяснить является ли число четным

---
+++ DZ13/Program.cs
@@ -0,0 +1,14 @@
+﻿// Являетя ли число четным
+int a = new Random().Next(1,10);
+Console.WriteLine(a);
+
+if(a %2 == 0) 
+{
+  Console.Write("Число четное ");
+  Console.Write(a);
+}
+else
+{
+  Console.Write(" Число не четное ");
+  Console.Write(a);
+}
