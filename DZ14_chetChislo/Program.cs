c35825d4c2e8f84c82206dfae0ae4bdd14a1d3f7
Yanina063 <strrrekosa@mail.ru>
2022-05-21 14:24:55 +0400

    Все четные числа до n

---
+++ DZ14_chetChislo/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать четные числа от 1 до N
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Все четные числа до N: ");
+for(int i = 1; i <= N; i++) 
+{
+  if(i % 2 == 0) {
+      Console.Write($"{i}, ");
+     
+  }
+}
