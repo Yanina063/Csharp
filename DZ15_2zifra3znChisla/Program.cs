53a4becee7926b6dbb275a5d6fea050ae926a4b1
Yanina063 <strrrekosa@mail.ru>
2022-05-21 15:08:27 +0400

    Вторая цифра 3хзначного числа

---
+++ DZ15_2zifra3znChisla/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать вторую цыфру 3хзначного числа
+Console.Write("Введите трехзначное число: ");
+ int a = int.Parse(Console.ReadLine());
+ if (a > 99 && a < 1000)
+ 
+ 
+ 
+ {
+   Console.Write("Вторая цифра 3-х значного числа = ");
+   Console.WriteLine(a / 10 % 10);  
+ }
+else {
+    Console.Write("Число не 3-х значное");
+}
