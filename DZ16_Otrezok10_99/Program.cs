25bf825d5872cdd18fe2b9d29416866d9b581674
Yanina063 <strrrekosa@mail.ru>
2022-05-21 15:51:39 +0400

    Удалить вторую цыфру 3хзначного числа

---
+++ DZ16_Otrezok10_99/Program.cs
@@ -0,0 +1,20 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа.
+int a = int.Parse(Console.ReadLine());
+int b = a / 10 % 10;
+int c = a % 10; 
+if (a > 10 && a < 99)
+if(b > c)
+{
+    Console.Write("Наибольшая цифра числа: ");
+    Console.WriteLine(b);
+}
+else
+{
+    Console.Write("Наибольшая цифра числа: ");
+    Console.WriteLine(c);
+} 
+
+
+
+
+
---
+++ DZ17_Del2cifruChisla/Program.cs
@@ -0,0 +1,15 @@
+﻿// Удалить вторую цыфру 3хзначного числа
+Console.Write("Введите трехзначное число: ");
+int a = int.Parse(Console.ReadLine());
+int b = a / 100;
+int c = a % 10;
+int d = b * 10 + c;
+if (a > 99 && a < 1000)
+{
+   Console.Write("Вторая цифра 3-х значного числа = ");
+   Console.WriteLine(a / 10 % 10);  
+}
+else {
+    Console.Write("Число не 3-х значное");
+}
+Console.WriteLine(d);
