df53ac53b2fcd3055763dbb3a189cca499a06325
Yanina063 <strrrekosa@mail.ru>
2022-05-21 16:13:39 +0400

    Выяснить, кратно ли число заданному, если нет, вывести остаток

---
+++ DZ18_KratnoeChislo/Program.cs
@@ -0,0 +1,18 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+Console.Write("Введите число: ");
+double a = int.Parse(Console.ReadLine());
+Console.Write("Введите число: ");
+double b = int.Parse(Console.ReadLine());
+double c = b % a;
+
+if(c == 0)
+{
+   Console.WriteLine($"Число {b} кратное: {a}");  
+}
+else 
+{
+    Console.WriteLine($"Число {b} не кратное: {a}");
+}
+Console.WriteLine($"Остаток: {c}");
+
+
