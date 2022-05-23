c43edb63c46d3c42b104e74eb9488274ef85fdfa
Yanina063 <strrrekosa@mail.ru>
2022-05-23 12:37:14 +0400

    Добавить решение в строку

--- Practic52/Program.cs
+++ Practic52/Program.cs
@@ -1,17 +1,4 @@
-﻿// Перевести десятичное число в двоичное
-//Написать программу преобразования десятичного числа в двоичное
-Console.WriteLine("Number?");
-int number = int.Parse(Console.ReadLine());
-int tmpnumber = number;
-int tmp = 0;
-int count = 0;
-while(tmpnumber > 0)
-    {
-        tmp = tmpnumber % 2;
-        tmpnumber = tmpnumber /2;
-        count++;
-    }
-void FillArray(int[] array) 
+﻿void  FillArray(int[] array)
 {
     int tmp = 0;
     int index = 0;
@@ -35,7 +22,38 @@ void PrintArray(int[] col)
         position++;
     }
 }
+//Написать программу преобразования десятичного числа в двоичное
+Console.WriteLine("Number?");
+int number = int.Parse(Console.ReadLine());
+int tmpnumber = number;
+int tmp = 0;
+int count = 0;
+while(tmpnumber > 0) //считаем какое кол-во цифр нам потребуется для деления числа
+    {
+        tmp = tmpnumber % 2;
+        tmpnumber = tmpnumber /2;
+        count++;
+    }
+
 int[] array = new int[count];
 FillArray(array);
 PrintArray(array.Reverse().ToArray());
 
+
+//Написать программу преобразования десятичного числа в двоичное.
+//Решение в строку
+
+//Console.WriteLine("Введите число");
+//int a = int.Parse(Console.ReadLine());
+//int b = 0;
+//string result = " ";
+//int result = 0;
+
+//while(a > 0)
+//{
+    b = a % 2;
+    result = b + result;
+    a = a / 2;
+  
+//
+//Console.WriteLine(result);
