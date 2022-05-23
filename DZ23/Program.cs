d9e1cc75080fb7302033989ab6c5dc2803a1cf08
Yanina063 <strrrekosa@mail.ru>
2022-05-23 18:36:37 +0400

    Четверть и данные

---
+++ DZ23/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задать номер четверти. Показать диапазон возможных координат.
+Console.WriteLine("Введите номер четверти: ");
+int Plase = int.Parse(Console.ReadLine());
+
+if(Plase == 1) 
+{
+Console.WriteLine("точка находится в Плоскости 1");
+Console.WriteLine("Имеет все значение от x > 0 и y > 0");
+} else if(Plase == 2) 
+{
+Console.WriteLine("точка находится в Плоскости 2");
+Console.WriteLine("Имеет все значение от x > 0 и y < 0");
+} else if(Plase == 3) 
+{
+Console.WriteLine("точка находится в Плоскости 3");
+Console.WriteLine("Имеет все значение от x < 0 и y < 0");
+} else if(Plase == 4) 
+{
+Console.WriteLine("точка находится в Плоскости 4");
+Console.WriteLine("Имеет все значение от x < 0 и y > 0");
+} else {
+Console.WriteLine("Ошибка ввода координат");
+}
