57e3c676ff5f91e3d52722e658e0c96af43f73c8
Yanina063 <strrrekosa@mail.ru>
2022-05-23 13:16:02 +0400

    Вычисляем ряд Фебоначе

---
+++ Practic53_Febonache/Program.cs
@@ -0,0 +1,34 @@
+﻿// Показать числа Фибоначчи меньше заданного числа N
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()?? "");
+
+int firstValue  = 0;
+int secondValue = 1;
+int result      = 0;
+Console.Write($"{firstValue} {secondValue} ");
+
+while(result < N)
+{
+    result = firstValue + secondValue;
+    if(result > N) break;
+    Console.Write(result + " ");
+    firstValue = secondValue;
+    secondValue = result;
+}
+
+
+//Показать числа Фибоначчи меньше заданного числа N
+
+Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+int a = 1;
+int b = 0;
+int result;
+for (int i =0;i < n;i++)
+{
+    result = a + b;
+    Console.WriteLine(result + " ");
+    a = b;
+    b = result;
+}
+
