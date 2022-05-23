6d986f8ab0621d83699533261c1579cc52ec9d7d
Yanina063 <strrrekosa@mail.ru>
2022-05-23 11:06:34 +0400

    Проверка числа на квадрат

---
+++ DZ22/Program.cs
@@ -0,0 +1,16 @@
+﻿// Выяснить, является ли число квадратом ддругого
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число: ");
+int b = int.Parse(Console.ReadLine());
+int c = a * a;
+
+if(c == b)
+{
+   Console.WriteLine($"Число {b} является квадратом: {a}");  
+}
+else 
+{
+    Console.WriteLine($"Число {b} не не является квадратом: {a}");
+}
+
