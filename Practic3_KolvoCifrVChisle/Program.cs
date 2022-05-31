aae90f91d0af23a03a8d105015c97a159454b55a
Yanina063 <strrrekosa@mail.ru>
2022-05-31 16:29:14 +0400

    кол-во цифр в числе

---
+++ Practic3_KolvoCifrVChisle/Program.cs
@@ -0,0 +1,11 @@
+﻿// Определить кол-во цифр в числе
+Console.WriteLine("Ввести число number: ");
+int number = int.Parse(Console.ReadLine());
+int count = 0;
+int tmpnumber = number;
+while(tmpnumber > 0)
+{
+    tmpnumber = tmpnumber / 10;
+    count++;
+}
+Console.WriteLine($"Кол-во цифр {count} в числе {number}");
