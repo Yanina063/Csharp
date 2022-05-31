39b2d4576e04912a50dc1fdf2e29311ed972f6cf
Yanina063 <strrrekosa@mail.ru>
2022-05-31 14:07:26 +0400

    Возвести число А в степень В

---
+++ DZ34_AvBstepen/Program.cs
@@ -0,0 +1,13 @@
+﻿//Возвести число A в натуральную степень B используя цикл
+Console.WriteLine("Введите число A: ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int B = int.Parse(Console.ReadLine());
+
+int init_A = A;
+
+for (int i = 1; i < B; i++)
+{
+    A = A * init_A;
+}
+Console.WriteLine($"B = {B} A = {A}");
