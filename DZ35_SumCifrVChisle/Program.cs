52f32509f86208ee7fd302c713ea8753bc67a029
Yanina063 <strrrekosa@mail.ru>
2022-05-31 14:39:39 +0400

    Подсчитать сумму цифр в числе

---
+++ DZ35_SumCifrVChisle/Program.cs
@@ -0,0 +1,15 @@
+﻿//Посчитать сумму цифр в числе
+Console.WriteLine("Введите число A: ");
+int A = int.Parse(Console.ReadLine());
+
+int sum = A % 10;
+
+Console.Write($"sum = {sum}");
+
+for (int i = A / 10; i > 0; i = i / 10)
+{
+    int ost = i % 10;
+    sum = sum + ost;
+    Console.Write($" + {ost}");
+}
+Console.Write($" = {sum}");
