ea761c0175366040cba48d92619454b2611c27c6
Yanina063 <strrrekosa@mail.ru>
2022-05-31 13:30:36 +0400

    Найти кубы чисел от 1 до N

---
+++ DZ32_Tabl3Chisel/Program.cs
@@ -0,0 +1,7 @@
+﻿//Показать таблицу кубов чисел от 1 до N
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+{
+    Console.WriteLine($"{i} * {i} * {i} = {i * i * i}");
+}
