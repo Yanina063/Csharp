bbf84ba573a5928f877af66a1b959fb4b327b5e3
Yanina063 <strrrekosa@mail.ru>
2022-05-31 17:03:37 +0400

    Задать массив из 8 элементов

---
+++ Practic3_ZadatMassiv/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задать массив из 8 элементов и вывести их на экран
+Console.WriteLine("Введите 8 элементов");
+int[] array = new int[8];  //задаем сам массив
+for(int i = 1; i <=8; i++)
+{
+    Console.WriteLine($"Введите элемент номер {i}: \n");
+    array[i-1] = int.Parse(Console.ReadLine());
+}
+Console.WriteLine();
+PrintArray(array);
+//----------------------Функция ввода массива на экран--------------------
+void PrintArray(int[] array)
+{
+    for(int i = 1; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+//------------------------------------------------------------------------
