9d5ead1d9b070343a9339e5442bdd8dcca5b6673
Yanina063 <strrrekosa@mail.ru>
2022-05-31 16:37:34 +0400

    Произведение чисел от 1 до N

---
+++ Practic3_ProizvedenieChisel/Program.cs
@@ -0,0 +1,7 @@
+﻿//Произведение чисел от 1 до N
+Console.WriteLine("Введите число: ");
+int P = int.Parse(Console.ReadLine());
+for (int i = 0; i <= P - 1; i++)
+{
+    Console.WriteLine($"{i} * {i + 1} = {i * (i + 1)}");
+}
