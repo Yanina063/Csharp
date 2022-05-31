efca52e85d9c0f19e45e735dd40785d741efe062
Yanina063 <strrrekosa@mail.ru>
2022-05-31 13:38:31 +0400

    Найти сумму чисел от 1 до А

---
+++ DZ33_Sum1doA/Program.cs
@@ -0,0 +1,7 @@
+﻿//Найти сумму чисел от 1 до A
+Console.WriteLine("Введите число: ");
+int A = int.Parse(Console.ReadLine());
+for (int i = 0; i <= A - 1; i++)
+{
+    Console.WriteLine($"{i} + {i + 1} = {i + (i + 1)}");
+}
