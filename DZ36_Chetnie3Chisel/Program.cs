da9fc5edea273e99f633a178b61366083ed79d4d
Yanina063 <strrrekosa@mail.ru>
2022-05-31 18:35:46 +0400

    Массив 8 элементов, заполняем 1 и 0

---
+++ DZ36_Chetnie3Chisel/Program.cs
@@ -0,0 +1,28 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+//1. Показать таблицу кубов чисел от 1 до N
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+{
+    Console.WriteLine($"{i} * {i} * {i} = {i * i * i}");
+}
+
+Console.WriteLine("Введите n элементов");
+int[] array =new int[n];  //задаем сам массив
+for(int i = 1; i <=n; i++)
+{
+    Console.WriteLine($"Введите элемент номер {i}: ");
+    array[i-1] = int.Parse(Console.ReadLine());
+}
+Console.WriteLine();
+PrintArray(array);
+//-----------Функция ввода четных чисел массива на экран-----------------
+void PrintArray(int[] array)
+{
+    for(int i = 1; i < array.Length; i = i + 2)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+//------------------------------------------------------------------------
---
+++ DZ37_MassivZeroOne/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и еденицами. Вывести их на экран.
+
+//Задать массив из 8 элементов и вывести их на экран
+Console.WriteLine("Введите 8 элементов");
+int[] array = new int[8];  //задаем сам массив
+for(int i = 1; i <= 8; i++)
+{
+    Console.WriteLine($"Введите элемент номер {i}: \n");
+    array[i-1] = int.Parse(Console.ReadLine());
+}
+Console.WriteLine();
+PrintArray(array);
+
+//----------------------Функция ввода массива на экран--------------------
+void PrintArray(int[] array)
+{
+    for(int i = 1; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+//------------------------------------------------------------------------
+
+//2/Вывести на экран значения 0 и 1
+Console.WriteLine();
+PrintArray1(array);
+
+bool IsZeroOrOne(int number) {
+    int lastNumber = number % 10;
+    return lastNumber == 0 || lastNumber == 1; 
+}
+
+//-----------Функция ввода чисел массива с 1 и 0 на экран-----------------
+void PrintArray1(int[] array)
+{
+    for(int i = 0; i < array.Length; i = i + 1)
+    {
+        if(IsZeroOrOne(array[i]))
+        Console.Write(array[i] + " ");
+    }
+}
+//--------------------------------------------------------------------------
