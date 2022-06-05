49c8a823642a9fe685035dc6bcb4fa2f92197718
Yanina063 <strrrekosa@mail.ru>
2022-06-05 18:08:37 +0400

    Считаем нечетные позиции массива

---
+++ Practic42_SummaNumbersMassiv/Program.cs
@@ -0,0 +1,11 @@
+﻿//Определить сумму чисел одномерного массива, стоящих на нечетной позиции
+int[] arr = new int [8];
+for (int i = 0; i < arr.Length; i++) arr[i] = new Random().Next(1,10);
+int sum = 0;
+for (int i = 0; i < arr.Length; i++) Console.Write($"{arr[i]} ");
+Console.WriteLine();
+for (int i = 0; i < arr.Length; i = i + 2)
+{
+    sum = sum + arr[i];
+}
+Console.WriteLine($"Сумма нечетных элементов массива равна {sum} ");
