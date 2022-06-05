6ed09a0f9f25d48894e1a8db1eae47ea57933a84
Yanina063 <strrrekosa@mail.ru>
2022-06-05 18:25:37 +0400

    Разница между max и min вещественных чисел

---
+++ Practic43_MaxMinMassiv/Program.cs
@@ -0,0 +1,16 @@
+﻿//В указанном диапазоне вещественных чисел найдите разницу между максимальным и минимальным элементом
+float [] arr = {3.5f, 4.5f, 5.8f, 9.1f};
+float max = arr[1];
+float min = arr[0];
+for (int i = 0; i < arr.Length; i++)
+{ 
+    if(arr[i] > max)
+    {
+        max = arr[i];
+    }
+    if(arr[i] < min)
+    {
+        min = arr[i];
+    }
+}
+Console.WriteLine($"Разница между {max} и {min} = {max-min}");
