75f6019e210f5c64c9e5a8e50e8fc387fe4d7d7e
Yanina063 <strrrekosa@mail.ru>
2022-06-05 17:52:08 +0400

    Найти число в массиве

---
+++ Practic41_FindNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿//Определить, присутствует ли число в массиве
+int[] arr = {-3, 1, 59, 40, 12, 9, 0, 5};
+for (int i = 0; i < arr.Length; i++) Console.Write($"{arr[i]} ");
+Console.WriteLine();
+Console.WriteLine("Введите число для поиска в массиве: ");
+int num = int.Parse (Console.ReadLine()  ?? "0");
+int check = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] == num)
+    {
+        Console.WriteLine($"Заданное число {num} найдено в массиве!");
+        check = 1;
+        break;
+    }
+}
+if (check == 0) Console.WriteLine($"Заданное число {num} не найдено в массиве.");
