d1bc4f276a3e9a778a8c977e21ef188dbfb2d8e2
Yanina063 <strrrekosa@mail.ru>
2022-06-10 22:15:20 +0400

    Через функцию и массив

--- DZ52_Mashtabirovanie/Program.cs
+++ DZ52_Mashtabirovanie/Program.cs
@@ -1,25 +1,58 @@
 ﻿// Написать программу масштабирования фигуры (прямоугольник)
 
-int x1 = 5;
-int x2 = 10;
-int x3 = 15;
-int x4 = 7;
+int [][] square = new int[4][];
+square[0] = new int[2];
+square[1] = new int[2];
+square[2] = new int[2];
+square[3] = new int[2]; 
 
-int y1 = 5;
-int y2 = 10;
-int y3 = 15;
-int y4 = 20;
+void PrintArray(int [][] array) {
 
-Console.Write("Введите коэффициент: ");
+    Console.WriteLine("Результат Массив: ");
+
+    for(int i = 0; i < array.Length; i++) {
+      
+        for(int j = 0; j < array[i].Length; j++) {
+
+               Console.Write(" " + array[i][j]);
+                 
+        }
+    }
+}
+
+int [][] InputArray(int [][] array) {
+    
+    for(int i = 0; i < array.Length; i++) {
+      
+        for(int j = 0; j < array[i].Length; j++) {
+
+                Console.WriteLine("Введите число: ");
+                int res = int.Parse(Console.ReadLine() ?? "0");
+                 array[i][j] = res;
+        }
+    }
+
+    return array;
+}
+
+int [][] MashtabArray(int [][] array, int k) {
+    
+    for(int i = 0; i < array.Length; i++) {
+      
+        for(int j = 0; j < array[i].Length; j++) {
+
+                 array[i][j] = array[i][j] * k;
+        }
+    }
+
+    return array;
+}
+
+int [][] filledArray = InputArray(square);
+
+Console.WriteLine("введите коэффициент: ");
 int k = int.Parse(Console.ReadLine() ?? "0");
-x1 = x1*k;
-x2 = x2*k;
-x3 = x3*k;
-x4 = x4*k;
 
-y1 = y1*k;
-y2 = y2*k;
-y3 = y3*k;
-y4 = y4*k;
+int [][] MashtabArrayResult = MashtabArray(filledArray, k);
 
-Console.WriteLine($"Новые координаты фигуры: x1: {x1}, y1: {y1}, x2: {x2}, y2: {y2}, x3: {x3}, y3: {y3}, x4: {x4}, y4: {y4}");
+PrintArray(MashtabArrayResult);
