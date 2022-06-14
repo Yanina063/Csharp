bd74d302c160582a45b3ea2ca1e6fa7722455c69
Yanina063 <strrrekosa@mail.ru>
2022-06-14 20:02:22 +0400

    A=m+n

---
+++ DZ62_MassivMN/Program.cs
@@ -0,0 +1,15 @@
+﻿//В двухмерном массиве заменить элементы, у которых оба индекса четные на их квадраты
+Console.Write("Введите кол-во строк 2хмерного массива: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите кол-во столбцов 2хмерного массива: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int[,] arr = new int[m + n,n + m];
+for(int i = 0; i < m + n; i++)
+{
+    for(int j = 0; j < m + n; j++)
+    {
+        arr[i,j] =  new Random().Next(0,20);
+        Console.Write($"{arr[i,j]} ");
+    }
+    Console.WriteLine();
+}
