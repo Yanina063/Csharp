80886ba51f95ab75446275dacea152a0d89c52d0
Yanina063 <strrrekosa@mail.ru>
2022-06-14 19:33:23 +0400

    Массив с вещественными числами

---
+++ DZ6/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать двумерный массив размером m×n заполненный вещественными числами
+Console.Write("Введите кол-во строк 2хмерного массива: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите кол-во столбцов 2хмерного массива: ");
+int k = int.Parse(Console.ReadLine() ?? "0");
+double[,] matrix = new double[n,k];
+for(int i = 0; i < n; i++)
+{
+    for(int j = 0; j < k; j++)
+    {
+        matrix[i,j] =  new Random().NextDouble();
+        Console.Write($"{matrix[i,j]} ");
+    }
+    Console.WriteLine();
+}
