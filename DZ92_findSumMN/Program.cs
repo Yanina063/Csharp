fa3c99040f5fc3ad450c565ddd64053bc26ba9d5
Yanina063 <strrrekosa@mail.ru>
2022-06-19 18:39:55 +0400

    Показать натуральные числа от m до N

---
+++ DZ92_findSumMN/Program.cs
@@ -0,0 +1,15 @@
+﻿//Найти сумму элементов от M до N, N и M заданы
+void PrintNums(int M, int N)
+{
+    int sum = M;
+    while(++M <= N) {
+        sum = sum + M;
+    }
+    Console.WriteLine(sum);
+}
+
+Console.Write("Введите первый элемент : ");
+int M = int.Parse(Console.ReadLine()?? "");
+Console.Write("Введите второй элемент : ");
+int N = int.Parse(Console.ReadLine()?? "");
+PrintNums(M,N);
