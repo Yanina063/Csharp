8e99af3bcf1f2e739ff146873aa59256c8343da0
Yanina063 <strrrekosa@mail.ru>
2022-06-19 18:41:07 +0400

    FindAllNumbersMN

---
+++ DZ91_FindAllNumbersMN/Program.cs
@@ -0,0 +1,22 @@
+﻿//Показать натуральные числа от M до N, N и M заданы
+void PrintNums(int M, int N, int I)
+{
+    if(I==N) return ;
+    Console.WriteLine(M);
+    int tmp = M;
+    M += 1;
+    PrintNums(M,N,I-1);
+}
+
+Console.Write("Введите число I: ");
+int I = int.Parse(Console.ReadLine()?? "");
+Console.Write("Введите первый элемент : ");
+int M = int.Parse(Console.ReadLine()?? "");
+Console.Write("Введите второй элемент : ");
+int N = int.Parse(Console.ReadLine()?? "");
+
+PrintNums(M,N,I);
+
+
+
+
